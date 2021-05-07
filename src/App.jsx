23d5aa016c8e7a8b1d79dedd44aae51d1bf0e42e
Andrerson Freitas <andrersonn@gmail.com>
2021-05-06 23:17:25 -0300

    Estado do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { RepositoryList } from './components/RepositoryList'
+import { Counter } from './components/Counter';
 import '../src/styles/global.scss'
 
 export function App(){
-    return <RepositoryList />
+    return (
+        <>
+            <RepositoryList />
+            <Counter />    
+        </>
+    );
 }
