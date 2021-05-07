a0588ab3ba9282cd76e290c3994f64c9efb2516b
Andrerson Freitas <andrersonn@gmail.com>
2021-05-07 00:09:03 -0300

    Utilizando o useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import { useState, useEffect } from "react";
+
 import '../styles/repositories.scss';
 
 const repository = {
@@ -9,6 +11,17 @@ const repository = {
 }
 
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+
+    //https://api.github.com/users/andrerson/repos
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+    
+    console.log(repositories);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
